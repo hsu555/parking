0eccc9c2a080c850271d7ff303ffa0531bd70dcd
Steve Hsu <hsu555@users.noreply.github.com>
2023-04-02 00:39:46 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,12 +2,13 @@ let distance = 0
 cuteBot.setServo(cuteBot.ServoList.S1, 90)
 basic.forever(function () {
     distance = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
-    basic.showString("" + (distance))
-    if (distance < 40) {
+    if (distance >= 2 && distance <= 30) {
         cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xffff00)
-        cuteBot.setServo(cuteBot.ServoList.S1, 90)
+        cuteBot.setServo(cuteBot.ServoList.S1, 0)
     } else {
-        cuteBot.setServo(cuteBot.ServoList.S1, 180)
+        cuteBot.setServo(cuteBot.ServoList.S1, 90)
         cuteBot.closeheadlights()
     }
+    basic.showString("" + (distance))
+    basic.pause(1000)
 })
